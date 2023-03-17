a1fc2beb60ea52ba63a837d3f8d4ab2b3eaab0db
Holographic <budninja36@gmail.com>
2023-03-17 01:56:19 -0400

    Placing the numbers

--- main.c
+++ main.c
@@ -31,6 +31,7 @@ typedef struct Cell
     int j;             // the index of the cell in the grid
     bool containsMine; // if the cell contains a mine
     bool revealed;     // if the cell is revealed
+    int nearbyMines;    // the number of mines nearby
 } Cell;
 
 // a two dimensional array of cells
@@ -41,6 +42,11 @@ Cell grid[COLS][ROWS]; // the grid of cells
 void CellDraw(Cell cell);        // Draw a cell
 bool IsIndexValid(int i, int j); // Check if the index is valid
 void CallReveal(int i, int j);   // Call the reveal function
+// Function to count the number of nearby mines per cell
+int CellCountMines(int i, int j); // Count the number of nearby mines per cell
+
+
+
 
 static void UpdateDrawFrame(void); // Update and draw one frame
 
@@ -56,16 +62,17 @@ int main()
     InitWindow(screenWidth, screenHeight, "MineSweeper");
 
     // Initialize the grid
-    for (int i = 0; i < COLS; i++) // Loop through the columns
+    for (int i = 0; i < COLS; i++)              // Loop through the columns
     {
-        for (int j = 0; j < ROWS; j++) // Loop through the rows
+        for (int j = 0; j < ROWS; j++)          // Loop through the rows
         {
-            grid[i][j] = (Cell) // Initialize the cell
+            grid[i][j] = (Cell)                 // Initialize the cell
                 {
-                    .i = i, // Set the index of the cell in the grid
-                    .j = j,  // Set the index of the cell in the grid
-                    .containsMine = false, // Set the cell to not contain a mine
-                    .revealed = false, // Set the cell to not be revealed
+                    .i = i,                     // Set the index of the cell in the grid
+                    .j = j,                     // Set the index of the cell in the grid
+                    .containsMine = false,      // Set the cell to not contain a mine
+                    .revealed = false,          // Set the cell to not be revealed
+                    .nearbyMines = -1            // Set the number of nearby mines to -1
                 };                
         }
     }
@@ -74,17 +81,26 @@ int main()
     int minesToPlace = (int)(ROWS * COLS * 0.1f); // the number of mines to place
     
     // Place the mines
-    while (minesToPlace > 0) // Loop until all the mines are placed
+    while (minesToPlace > 0)                    // Loop until all the mines are placed
     {
-        int i = rand() % COLS; // Get a random index for the column
-        int j = rand() % ROWS; // Get a random index for the row
+        int i = rand() % COLS;                  // Get a random index for the column
+        int j = rand() % ROWS;                  // Get a random index for the row
 
-        if (!grid[i][j].containsMine) // if the cell doesn't contain a mine
+        if (!grid[i][j].containsMine)           // if the cell doesn't contain a mine
         {
-            grid[i][j].containsMine = true; // Set the cell to contain a mine
-            minesToPlace--;                 // Decrement the number of mines to place
+            grid[i][j].containsMine = true;     // Set the cell to contain a mine
+            minesToPlace--;                     // Decrement the number of mines to place
         }
     }
+
+    //  Count the number of nearby mines per cell
+    for (int i = 0; i < COLS; i++)              // Loop through the columns
+    {
+        for (int j = 0; j < ROWS; j++)          // Loop through the rows
+        {
+            grid[i][j].nearbyMines = CellCountMines(i, j); // Count the number of nearby mines per cell
+        }
+    }  
 
     //--------------------------------------------------------------------------------------
 
@@ -133,11 +149,11 @@ static void UpdateDrawFrame(void)
     ClearBackground(RAYWHITE);
 
     // Draw the grid
-    for (int i = 0; i < COLS; i++) // Loop through the columns
+    for (int i = 0; i < COLS; i++)           // Loop through the columns
     {
-        for (int j = 0; j < ROWS; j++) // Loop through the rows
+        for (int j = 0; j < ROWS; j++)      // Loop through the rows
         {
-            CellDraw(grid[i][j]); // Draw the cell
+            CellDraw(grid[i][j]);           // Draw the cell
         }
     }
 
@@ -157,6 +173,12 @@ void CellDraw(Cell cell)
         else // if doesn't contain mine
         {
             DrawRectangle(cell.i * gridWidth, cell.j * gridHeight, gridWidth, gridHeight, LIGHTGRAY); // draw a light gray square
+            // if the clicked on cell doesn't contain a mine, draw the number of nearby mines
+            if (cell.nearbyMines > 0)
+            {
+                DrawText(TextFormat("%i", cell.nearbyMines), cell.i * gridWidth + 10, cell.j * gridHeight + 10, 20, BLACK);
+            }
+
         }
     }
 
@@ -167,15 +189,15 @@ void CellDraw(Cell cell)
 // Check if the index is valid
 bool IsIndexValid(int i, int j)
 {
-    return i >= 0 && i < COLS && j >= 0 && j < ROWS; // return true if the index is valid
+    return i >= 0 && i < COLS && j >= 0 && j < ROWS;    // return true if the index is valid
 }
 
 // reveal the cell and if it contains a mine, lose the game
 void CallReveal(int i, int j)
 {
-    grid[i][j].revealed = true; // reveal the cell
+    grid[i][j].revealed = true;     // reveal the cell
 
-    if (grid[i][j].containsMine) // if the cell contains a mine
+    if (grid[i][j].containsMine)    // if the cell contains a mine
     {
         // Lose the game
     }
@@ -183,4 +205,30 @@ void CallReveal(int i, int j)
     {
         // TODO: play a sound.
     }
+}
+
+// Count the number of nearby mines per cell
+int CellCountMines(int i, int j)
+{
+    int count = 0; // the number of nearby mines
+
+    // Loop through the nearby cells
+    for (int x = -1; x <= 1; x++) // Loop through the columns
+    {
+        for (int y = -1; y <= 1; y++) // Loop through the rows
+        {
+            int nearbyI = i + x; // Get the nearby index of the cell in the grid
+            int nearbyJ = j + y; // Get the nearby index of the cell in the grid
+
+            if (IsIndexValid(nearbyI, nearbyJ)) // Check if the index is valid
+            {
+                if (grid[nearbyI][nearbyJ].containsMine) // if the cell contains a mine
+                {
+                    count++; // Increment the number of nearby mines
+                }
+            }
+        }
+    }
+
+    return count; // return the number of nearby mines
 }
